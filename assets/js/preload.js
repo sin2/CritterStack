4a85cf52e0c692aff18b2fbde4754d3790d2a147
Ty Rozak <tyrozak@gmail.com>
2012-06-01 14:32:33 -0400

    Changes to preload

--- assets/js/preload.js
+++ assets/js/preload.js
@@ -11,18 +11,28 @@
 		
 		preload.onComplete = doneLoading;
 		preload.installPlugin(SoundJS);
-			preload.loadFile("assets/images/blue1.png");
-			preload.loadFile("assets/images/blue2.png");
-			preload.loadFile("assets/images/blue3.png");
+		
+		var manifest = [			
+			{id:"blue1", src:"assets/images/blue1.png"},
+			{id:"blue2", src:"assets/images/blue2.png"},
+			{id:"blue3", src:"assets/images/blue3.png"},
 			
-			preload.loadFile("assets/images/green1.png");
-			preload.loadFile("assets/images/green2.png");
-			preload.loadFile("assets/images/green3.png");
+			{id:"green1", src:"assets/images/green1.png"},
+			{id:"green2", src:"assets/images/green2.png"},
+			{id:"green3", src:"assets/images/green3.png"},
 			
-			preload.loadFile("assets/images/pink1.png");
-			preload.loadFile("assets/images/pink2.png");
-			preload.loadFile("assets/images/pink3.png");
+			{id:"pink1", src:"assets/images/pink1.png"},
+			{id:"pink2", src:"assets/images/pink2.png"},
+			{id:"pink3", src:"assets/images/pink3.png"},
+			
+			{id:"spaceship", src:"assets/images/spaceship.png"},
+			{id:"fullBack", src:"assets/images/background.png"},
+			{id:"bg1", src:"assets/images/bg1.png"},
+			{id:"bg2", src:"assets/images/bg2.png"},
+			{id:"bg3", src:"assets/images/bg3.png"},
+			{id:"bg4", src:"assets/images/bg4.png"}];
 		//preload.onProgress = updateLoading;
+		preload.loadFile("assets/images/blue1.png");
 
 		canvas = document.getElementById("canvas");
 		stage = new Stage(canvas);
@@ -33,8 +43,8 @@
 		var img = new Image();
 		img.src = "assets/images/splash.png";
 		img.onload = function(e){
-			 splash = new Bitmap(e.target);
-			stage.addChild(splash, messageField);
+		splash = new Bitmap(e.target);
+		stage.addChild(splash, messageField);
 		stage.update();
 			}
 			
